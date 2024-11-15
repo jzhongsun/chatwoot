2585b5fad8ea11bc5945e2ca36b3a62a18616ce9
孙建忠 <jzhongsun@foxmail.com>
2024-11-15 15:44:11 +0800

    update dev

--- vite.config.dev.ts
+++ vite.config.dev.ts
@@ -33,6 +33,29 @@ const csrf_token = 'NKgyM8tY6awKD9iZyQZewynrZmGBpfyLHISbr_FevuQa7dkRyZ38l2vpp8dg
 
 const backend = 'http://localhost:8080';
 export function mpa_route_plugin() {
+
+  const htmlHandleApp = function (html, app) {
+    html = html.replace('{{{global_config.INSTALLATION_NAME}}}', app.global_config.INSTALLATION_NAME)
+    html = html.replace('{{app_config.HELP_CENTER_URL}}', app.app_config.HELP_CENTER_URL)
+    html = html.replace('{{global_config.FB_APP_ID}}', app.global_config.FB_APP_ID)
+
+    html = html.replace('{{app_config.GOOGLE_OAUTH_CLIENT_ID}}', app.app_config.GOOGLE_OAUTH_CLIENT_ID)
+    html = html.replace('{{app_config.GOOGLE_OAUTH_CALLBACK_URL}}', app.app_config.GOOGLE_OAUTH_CALLBACK_URL)
+
+    html = html.replace('{{global_config.FACEBOOK_API_VERSION}}', app.global_config.FACEBOOK_API_VERSION)
+    html = html.replace('{{global_config.ENABLE_ACCOUNT_SIGNUP}}', app.global_config.ENABLE_ACCOUNT_SIGNUP)
+    html = html.replace('{{global_config.IS_ENTERPRISE}}', app.global_config.IS_ENTERPRISE)
+    html = html.replace('{{app_config.INSTALLATION_PRICING_PLAN}}', app.app_config.INSTALLATION_PRICING_PLAN)
+
+    html = html.replace('{{{json app_config.AVAILABLE_LANGUAGES}}}', JSON.stringify(app.app_config.AVAILABLE_LANGUAGES))
+    html = html.replace('{{{json app_config.FEATURE_HELP_URLS}}}', JSON.stringify(app.app_config.FEATURE_HELP_URLS))
+    html = html.replace('{{app_config.LOCALE}}', app.app_config.LOCALE)
+
+
+    html = html.replace('{{{json global_config}}}', JSON.stringify(app.global_config))
+    html = html.replace('{{{json browser_config}}}', JSON.stringify(app.browser_config))
+    return html;
+  };
   return {
     name: 'route_plugin',
     apply: 'serve',
@@ -40,32 +63,74 @@ export function mpa_route_plugin() {
       server.middlewares.use(async (req, res, next) => {
         // 自定义请求处理...
         // console.log('req.url', req.url);
-        if (req.url === '/app/' || req.url === '/') {
-          let html = fs.readFileSync(path.resolve(__dirname, 'dashboard.html'), {encoding: 'utf-8'})
-          res.end(html);
-        } else if (req.url === '/app/login') {
-          let html = fs.readFileSync(path.resolve(__dirname, 'v3app.html'), {encoding: 'utf-8'})
-          res.end(html);
-        } else if (req.url?.startsWith('/widget?website_token=')) {
-          const config = await fetch(backend + req.url, {
+        if (req.url === '/app/login' || req.url === '/auth') {
+          const config = await fetch(backend + '/dashboard', {
             method: 'GET',
             headers: {
               'Content-Type': 'application/json'
             },
           });
-          console.info('config', config);
-          const widget = await config.json();
-          let html = fs.readFileSync(path.resolve(__dirname, 'widget.html'), {encoding: 'utf-8'})
-          html = html.replace('{{installation_name}}', widget.global_config.installation_name)
-          html = html.replace('{{csrf_token}}', csrf_token)
-          html = html.replace('{{pubsub_token}}', widget.contact_inbox.pubsub_token)
-          html = html.replace('{{auth_token}}', widget.conversation_token)
-          html = html.replace('{{global_config}}', JSON.stringify(widget.global_config))
-          html = html.replace('{{web_channel}}', JSON.stringify(web_channel))
-          res.end(html);
+          const app = await config.json();
+          let html = fs.readFileSync(path.resolve(__dirname, 'v3app.html'), {encoding: 'utf-8'})
+          html = htmlHandleApp(html, app);
+          res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' }).end(html);
         } else if (req.url?.startsWith('/app/accounts')) {
+          const config = await fetch(backend + '/dashboard', {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+          });
+          const app = await config.json();
+          console.info('config', app);
           let html = fs.readFileSync(path.resolve(__dirname, 'dashboard.html'), {encoding: 'utf-8'})
-          res.end(html);
+          html = htmlHandleApp(html, app);
+          res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' }).end(html);
+        } else if (req.url?.startsWith('/widget?website_token=')) {
+          const config = await fetch(backend + req.url, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'Cookie': req.headers['cookie'],
+              'User-Agent': req.headers['user-agent'],
+            },
+          });
+          console.info('config', config);
+          const app = await config.json();
+          let html = fs.readFileSync(path.resolve(__dirname, 'widget.html'), {encoding: 'utf-8'})
+          html = html.replace('{{{global_config.INSTALLATION_NAME}}}', app.global_config.INSTALLATION_NAME)
+
+          html = html.replace('{{inbox.avatar_url}}', app.inbox.avatar_url)
+          html = html.replace('{{agent_bot.name}}', app.agent_bot.name)
+          html = html.replace('{{account.locale}}', app.account.locale)
+
+          html = html.replace('{{inbox.name}}', app.inbox.name)
+          html = html.replace('{{inbox.working_hours_enabled}}', app.inbox.working_hours_enabled)
+          html = html.replace('{{inbox.csat_survey_enabled}}', app.inbox.csat_survey_enabled)
+          html = html.replace('{{inbox.out_of_office_message}}', app.inbox.out_of_office_message)
+          html = html.replace('{{inbox.allow_messages_after_resolved}}', app.inbox.allow_messages_after_resolved)
+          html = html.replace('{{{json inbox.portal}}}', JSON.stringify(app.inbox.portal))
+          html = html.replace('{{{json inbox.feature_flags}}}', JSON.stringify(app.inbox.feature_flags))
+          html = html.replace('{{{json inbox.working_hours}}}', JSON.stringify(app.inbox.working_hours))
+
+          html = html.replace('{{web_widget.website_token}}', app.web_widget.website_token)
+          html = html.replace('{{web_widget.welcome_tagline}}', app.web_widget.welcome_tagline)
+          html = html.replace('{{web_widget.welcome_title}}', app.web_widget.welcome_title)
+          html = html.replace('{{web_widget.widget_color}}', app.web_widget.widget_color)
+          html = html.replace('{{web_widget.reply_time}}', app.web_widget.reply_time)
+          html = html.replace('{{web_widget.pre_chat_form_enabled}}', app.web_widget.pre_chat_form_enabled)
+
+
+          html = html.replace('{{{json languages}}}', JSON.stringify(app.languages))
+
+
+          html = html.replace('{{account.features.disable_branding}}', app.features.disable_branding)
+          html = html.replace('{{contact_inbox.pubsub_token}}', app.contact_inbox.pubsub_token)
+          html = html.replace('{{auth_token}}', app.auth_token)
+
+          html = html.replace('{{{json global_config}}}', JSON.stringify(app.global_config))
+          html = html.replace('{{{json browser_config}}}', JSON.stringify(app.browser_config))
+          res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' }).end(html);
         } else {
           next();
         }
