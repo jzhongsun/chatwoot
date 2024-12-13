2184f55e2585ee54e0481148616d3b8b02e93f9e
孙建忠 <jzhongsun@foxmail.com>
2024-12-13 23:10:04 +0800

    update token

--- app/javascript/widget/helpers/axios.js
+++ app/javascript/widget/helpers/axios.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 import { APP_BASE_URL } from 'widget/helpers/constants';
 
+axios.interceptors.response.use(response => {
+  if (response.headers.get('X-Auth-Token')) {
+    setHeader(response.headers.get('X-Auth-Token'));
+  }
+});
+
 export const API = axios.create({
   baseURL: APP_BASE_URL,
   withCredentials: false,
@@ -8,6 +14,7 @@ export const API = axios.create({
 
 export const setHeader = (value, key = 'X-Auth-Token') => {
   API.defaults.headers.common[key] = value;
+  window.authToken = response.headers.get('X-Auth-Token');
 };
 
 export const removeHeader = key => {
