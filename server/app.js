63e2bd3ceed757728dd52313cc95f9a90172073f
孙建忠 <jzhongsun@foxmail.com>
2024-12-14 23:28:48 +0800

    add

--- server/app.js
+++ server/app.js
@@ -7,6 +7,12 @@ var proxy = require('http-proxy-middleware');
 const { randomUUID } = require('crypto');
 
 var app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var router = express.Router();
 
 var env = process.env;
