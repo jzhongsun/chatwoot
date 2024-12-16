3bd7a391f6f22ffe8a6ddee5ab9a60a45109cefd
孙建忠 <jzhongsun@foxmail.com>
2024-12-16 11:08:39 +0800

    update logo

--- server/app.js
+++ server/app.js
@@ -193,7 +193,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../', 'public')));
 app.use(express.static(path.join(__dirname, '../', 'dist')));
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use(express.static(path.join(__dirname, 'assets')));
 
 app.use('/', router);
 
